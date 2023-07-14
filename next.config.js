dadcdc2097a87fba7a3c93fec19d75971f37fdd4
mzulzidan <mzulzidan@gmail.com>
2023-07-15 07:05:25 +0800

    able to fetch data from local json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['s3-ap-southeast-1.amazonaws.com', "assets.loket.com", "loket-production-sg.s3.ap-southeast-1.amazonaws.com"],
+    },
+}
 
 module.exports = nextConfig
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-stone-50 text-stone-950`}>{children}</body>
     </html>
   )
 }
